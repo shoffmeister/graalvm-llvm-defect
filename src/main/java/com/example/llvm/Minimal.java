4d3e55ee377b1e5ae69261c3c9641d321f64b128
Stefan Hoffmeister <stefan.hoffmeister@econos.de>
2022-09-17 18:09:02 +0200

    Initial commit

---
+++ src/main/java/com/example/llvm/Minimal.java
@@ -0,0 +1,13 @@
+package com.example.llvm;
+
+public class Minimal {
+
+    static String computeOutputString() {
+        return "Hello world!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(computeOutputString());
+    }
+
+}
